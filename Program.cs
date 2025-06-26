09df951df4628c96b62071de8b143825fc558e50
Maros Beno <maros.beno@outlook.com>
2025-06-26 13:12:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using MudBlazor.Services;
+using MudBlazorElementRefException.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
